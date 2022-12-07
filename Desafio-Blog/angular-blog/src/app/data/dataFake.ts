7e3ef01e7a7932bc5d476a4d5c1ae3eedf929552
Thaysa Zooby <thaysa.zooby@gmail.com>
2022-12-07 08:06:29 -0300

    Desafio Blog completo

---
+++ Desafio-Blog/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id": '1',
+    "title": 'novo filme',
+    "description": 'ola',
+    "photoCover": "https://www.xfire.com/wp-content/uploads/2022/09/Netflix-Wednesday-Addams-Family-Thing-Nevermore-Academy-800x480.jpg"
+    
+  },
+  {
+    "id": '2',
+    "title": 'novo filme',
+    "description": 'ola',
+    "photoCover": "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
+  },
+];
